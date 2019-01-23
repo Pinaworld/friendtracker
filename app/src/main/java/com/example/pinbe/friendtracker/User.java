c6f3017f7d98bdc1c036f975f3634436e95e4cd2
ElCammel <lamygregoire1@gmail.com>
2019-01-23 19:55:29 +0100

    Adding Firebase Authentification to the application

---
+++ app/src/main/java/com/example/pinbe/friendtracker/User.java
@@ -0,0 +1,42 @@
+package com.example.pinbe.friendtracker;
+
+public class User {
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String firstname, String lastname, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
