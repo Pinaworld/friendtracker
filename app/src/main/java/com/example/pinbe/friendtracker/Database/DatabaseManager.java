40739321f576103bb42340bfd6445df9b0ed4e81
ElCammel <lamygregoire1@gmail.com>
2019-02-03 15:23:56 +0100

    database manager update

--- app/src/main/java/com/example/pinbe/friendtracker/Database/DatabaseManager.java
+++ app/src/main/java/com/example/pinbe/friendtracker/Database/DatabaseManager.java
@@ -1,15 +1,30 @@
 package com.example.pinbe.friendtracker.Database;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DatabaseManager {
-    private FirebaseDatabase firebaseInstance;
+    private DatabaseReference firebaseDatabase;
 
     public DatabaseManager(){
 
-        firebaseInstance = FirebaseDatabase.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseDatabase.child("friendTrackerDB").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
-
-
 
 }
