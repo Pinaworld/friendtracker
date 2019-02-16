658d3422b56bf6a9148a42e867b577e8c0c3a4db
ElCammel <lamygregoire1@gmail.com>
2019-02-16 15:47:19 +0100

    correction of default position

--- app/src/main/java/com/example/pinbe/friendtracker/Activities/MapsActivity.java
+++ app/src/main/java/com/example/pinbe/friendtracker/Activities/MapsActivity.java
@@ -17,8 +17,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.example.pinbe.friendtracker.ActivityIntentService;
